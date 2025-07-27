9dbeeb06a2b81086d5d487e9aafe1d37801f2ac2
Rafi <tahmidulrafi13@gmail.com>
2025-07-28 05:08:41 +0600

    Register page design done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
 import loginBg from '../../assets/Login/loginBg.png';
 import { FcGoogle } from "react-icons/fc";
 import { motion } from "motion/react"
 import { Link } from "react-router-dom";
 
-const Login = () => {
-  const handleSubmit = (e) => {
+const Register = () => {
+
+    const handleSubmit = (e) => {
     e.preventDefault();
+    const name = e.target.name.value;
     const email = e.target.email.value;
+    const photo = e.target.photo.value;
     const password = e.target.password.value;
-    console.log(email,password);
+    console.log(name,email,photo,password);
   };
 
-  return (
-    <div className="relative min-h-[calc(100vh-64px)] bg-gradient-to-b from-blue-50 to-cyan-100 flex flex-col items-center px-6 sm:px-12 pt-8 lg:pt-10 overflow-hidden">
+    return (
+        <div className="relative min-h-[calc(100vh-64px)] bg-gradient-to-b from-blue-50 to-cyan-100 flex flex-col items-center px-6 sm:px-12 pt-8 lg:pt-10 overflow-hidden">
 
       <img
         src={loginBg}
@@ -22,9 +25,9 @@ const Login = () => {
 
       />
 
-      <h2 className="text-xl xs:text-3xl md:text-4xl font-extrabold py-2 mb-4 bg-gradient-to-r from-sky-500 to-blue-400 drop-shadow-sm bg-clip-text text-transparent z-10">Login Here</h2>
+      <h2 className="text-xl xs:text-3xl md:text-4xl font-extrabold py-2 mb-4 bg-gradient-to-r from-sky-500 to-blue-400 drop-shadow-sm bg-clip-text text-transparent z-10">Create an Account</h2>
       <p className="text-sm text-sky-600 mb-8 z-10">
-        Welcome back! Please enter your details to continue.
+        Join WarmlyBD and make a difference this winter.
       </p>
         <motion.div
             initial={{ opacity: 0, y: 30 }}
@@ -39,11 +42,27 @@ const Login = () => {
       >
         <input
           className="mb-4 w-full max-w-[300px] py-2 rounded-lg bg-gradient-to-r from-white to-cyan-100 border border-sky-300 placeholder:text-gray-500 px-4 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-sky-400 focus:border-sky-500 transition"
+          placeholder="Name"
+          type="text"
+          name="name"
+          autoComplete="name"
+          aria-label="Full Name"
+        />
+        <input
+          className="mb-4 w-full max-w-[300px] py-2 rounded-lg bg-gradient-to-r from-white to-cyan-100 border border-sky-300 placeholder:text-gray-500 px-4 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-sky-400 focus:border-sky-500 transition"
           placeholder="Email"
           type="email"
           name="email"
           autoComplete="email"
           aria-label="Email address"
+        />
+        <input
+          className="mb-4 w-full max-w-[300px] py-2 rounded-lg bg-gradient-to-r from-white to-cyan-100 border border-sky-300 placeholder:text-gray-500 px-4 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-sky-400 focus:border-sky-500 transition"
+          placeholder="Photo URL"
+          type="text"
+          name="photo"
+          autoComplete="off"
+          aria-label="Photo URL"
         />
         <input
           className="mb-4 w-full max-w-[300px] py-2 rounded-lg bg-gradient-to-r from-white to-cyan-100 border border-sky-300 placeholder:text-gray-500 px-4 placeholder:text-sm"
@@ -54,7 +73,7 @@ const Login = () => {
         <input
           className="bg-gradient-to-r from-blue-400 to-cyan-400 hover:from-cyan-500 hover:to-blue-500 text-white shadow-md transition duration-300 px-8 rounded-md py-2 cursor-pointer text-sm font-semibold"
           type="submit"
-          value="Login"
+          value="Register"
         />
       </form>
       <div className="w-full max-w-[300px] h-px bg-gray-200 my-4" />
@@ -66,13 +85,13 @@ const Login = () => {
             <button className="flex items-center justify-center gap-2 w-full py-2 rounded-lg bg-white text-gray-700 border border-gray-300 hover:bg-gray-100 shadow"><FcGoogle  className="text-lg"/>Google</button>
         </div>
         <div className="w-full max-w-[300px]">
-            <p className="text-sm text-gray-600 mt-6">New to WarmlyBD? <Link to={"/register"} className="text-blue-600 text-base font-semibold underline hover:text-blue-800 transition">Register</Link></p>
+            <p className="text-sm text-gray-600 mt-6">Already have account? <Link to={"/login"} className="text-blue-600 font-semibold text-base underline hover:text-blue-800 transition">Login</Link></p>
         </div>
       </div>
         </motion.div>
       
     </div>
-  );
+    );
 };
 
-export default Login;
+export default Register;
